d911a85ba57395dc9929f0587c93ae3ef13c8d98
Edgerik Leguizamon <edgerik.leguizamon@gmail.com>
2017-06-18 11:10:44 -0600

    stash

--- ProyectoProgramado03-EstructurasDeDatos2017/src/Programa/CityPoliTablero.java
+++ ProyectoProgramado03-EstructurasDeDatos2017/src/Programa/CityPoliTablero.java
@@ -215,17 +215,9 @@ public class CityPoliTablero extends Thread {
         }
     }
     //Funciones---------------------------------------------------------------
-<<<<<<< HEAD
 
-    private void updateGrafo() {
-=======
-    public void start() 
-    {
-        run();
-    }
     private void updateGrafo()
     {
->>>>>>> b0330ab6432ecef622f826d61acba6af119deb28
         visualG.setControlador_grafo(controlador_grafo);
     }
 
--- ProyectoProgramado03-EstructurasDeDatos2017/src/Programa/IConstants.java
+++ ProyectoProgramado03-EstructurasDeDatos2017/src/Programa/IConstants.java
@@ -6,13 +6,8 @@ package Programa;
 public class IConstants 
 {
     //INICIALIZO CONSTANTES A USAR EN CITYPOLI
-<<<<<<< HEAD
     public static final int panelHeight = 500;
     public static final int panelWidth = 1350;
-=======
-    public static final int panelHeight = 800;
-    public static final int panelWidth = 1200;
->>>>>>> b0330ab6432ecef622f826d61acba6af119deb28
     public static final String radioBusqueda = "2000";
     public static final int menuHeight = 500;
     public static final int menuWidth = 600;
@@ -28,9 +23,5 @@ public class IConstants
     public static final double defaultRate = 4.0;
     public static final float porcentaje_llenado = (float) 2/3;
     public static final int sleep = 500;
-<<<<<<< HEAD
     public static final int cantidadRetos = 3;
-=======
-    public static final String ruta = "C:\\Users\\Usuario1\\Desktop\\GitHub\\CityPol-\\ProyectoProgramado03-EstructurasDeDatos2017\\src\\GamePlay\\perfiles.as";
->>>>>>> b0330ab6432ecef622f826d61acba6af119deb28
 }
